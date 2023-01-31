e24b1ca6ff46ec7f6551b93f24efa9615448b9a6
Matt Goodhart <matt.goodhart@willowtreeapps.com>
2023-01-31 09:42:19 -0500

    Remove ignored files

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matt Goodhart on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
